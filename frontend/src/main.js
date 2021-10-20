043d0da504d385ee7466f08059f48400dec76ace
J A <34893923+J-avery32@users.noreply.github.com>
2021-10-20 15:19:36 -0700

    bootstrap, chat prototype

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import App from './App.vue';
+import router from './router/index';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount('#app');
