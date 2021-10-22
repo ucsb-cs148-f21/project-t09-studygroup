ca310634fcba691f8d7086c77dbd8f67c5075db0
Kenneth Wang <kennethwang@umail.ucsb.edu>
2021-10-22 12:42:32 -0700

    added AdminPanel.vue to views directory

---
+++ frontend/src/views/AdminPanel.vue
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>The greatest news app ever</title>
+  </head>
+  <body>
+    <div class="container" id="app">
+      <h3 class="text-center">VueNews</h3> 
+    </div>
+  </body>
+</html>
