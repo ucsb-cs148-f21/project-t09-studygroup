819018ce1a30c3ad5b541d31846b2a24e3fdd47b
Jennice Xu <zhennixu@umail.ucsb.edu>
2021-10-24 14:39:01 -0700

    Added AdminPanel and Firebase functions

--- frontend/src/views/AdminPanel.vue
+++ frontend/src/views/AdminPanel.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="adminpanel">
     <h1>Admin Panel</h1>
-    <b-button block variant="primary">Refresh Quarter
-        <b-button
-            @click.native="getCourses(quarter)"
-            variant="primary">
-        </b-button>
+    <b-button block variant="primary" @click="getClasses()">Refresh Quarter
     </b-button>
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  methods: {
+    getClasses(){
+    axios.post('http://localhost:8082/study-group-148/us-central1/widgets/add-recent-classes')
+    },
+  },
+
+}
+</script>
+
