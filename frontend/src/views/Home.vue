0d8141391553cef3c35a343357e755bfd8411cc5
Anisha Kabir <ahsina.ribak@gmail.com>
2021-10-24 18:03:37 -0700

    query firestore for course id and display chat room link

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -14,16 +14,33 @@
           required
         ></b-form-input>
       </b-form-group>
-
       <b-button id="submit" type="submit" variant="primary">Submit</b-button>
     </b-form>
-    <!--<b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>--->
+    <ul id="chat-room-links">
+      <li v-for="link in links" :key="link.id">
+        <p v-html="link.message"></p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHcoFBHO8mYT_hnT8wjEqQqGtTSuFE9s0',
+  authDomain: 'study-group-148.firebaseapp.com',
+  projectId: 'study-group-148',
+  storageBucket: 'study-group-148.appspot.com',
+  messagingSenderId: '732562499999',
+  appId: '1:732562499999:web:f262480681942c6fdff5aa',
+  measurementId: 'G-T94E5J56CR',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 // @ is an alias to /src
 export default {
   data() {
@@ -31,13 +48,30 @@ export default {
       form: {
         course: '',
       },
+      links: [],
       show: true,
     };
   },
   methods: {
-    onSubmit(event) {
+    async onSubmit(event) {
+      this.links = []
       event.preventDefault();
-      alert(JSON.stringify(this.form));
+      const userInput = JSON.stringify(this.form);
+      const courseName = JSON.parse(userInput).course;
+
+      // search firebase for course
+      const q = query(collection(db, 'courses_20214'), where('courseID', '==', courseName));
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+        const courseLink = 'http://localhost:8080/class/' + doc.id;
+        console.log(courseLink);
+        this.links.push({message: "<a href='" + courseLink + "'>" + courseLink + "</a>"});
+      });
+      if(querySnapshot.empty){
+        this.links.push({message: "<p>Course Not Found</p>"});
+      }
     },
   },
 };
@@ -48,9 +82,17 @@ export default {
   height: 100px;
   width: 30%;
   padding-left: 80px;
-  padding-top: 60px;
+  padding-top: 40px;
+  padding-bottom: 60px;
 }
 #input-group-1 {
   padding-bottom: 20px;
+}
+li {
+  list-style-type: none;
+}
+#chat-room-links{
+  padding-top: 40px;
+  padding-left: 0px;
 }
 </style>
