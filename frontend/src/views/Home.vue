ef7ffa1942e240e74c54afd0bf032d89bcd381f7
J A <34893923+J-avery32@users.noreply.github.com>
2021-12-02 22:52:43 -0800

    fixed search page style

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,108 +1,119 @@
 <template>
   <div>
-    <div>
-      <b-carousel
-        id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
-        fade
-        background="#ababab"
-        img-width="1024"
-        img-height="480"
-        style="text-shadow: 1px 1px 2px #333"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-      >
-        <b-carousel-slide caption="Welcome to UCSB study group 😊!">
-          <template #img>
-            <img
-              class="d-block img-fluid w-100"
-              alt="image slot"
-              width="480"
-              height="1024"
-              src="@/assets/lagoon.jpg"
-            />
-          </template>
-        </b-carousel-slide>
+    <!--Carousel code copied and adapted from here https://bootstrap-vue.org/docs/components/carousel-->
+    <b-row class="justify-content-md-center">
+      <b-col cols="10">
+        <b-carousel
+          id="carousel-1"
+          v-model="slide"
+          :interval="4000"
+          controls
+          fade
+          background="#ababab"
 
-        <b-carousel-slide
-          caption="In our UCSB study group website, you can look for study partners
+          style="text-shadow: 1px 1px 2px #333"
+          @sliding-start="onSlideStart"
+          @sliding-end="onSlideEnd"
+        >
+          <b-carousel-slide caption="Welcome to UCSB study group 😊!">
+            <template #img>
+              <img
+                class="d-block img-fluid w-100"
+                alt="image slot"
+
+                src="@/assets/lagoon.jpg"
+              >
+            </template>
+          </b-carousel-slide>
+
+          <b-carousel-slide
+            caption="In our UCSB study group website, you can look for study partners
               in the class you enrolled in."
-        >
-          <template #img>
-            <img
-              class="d-block img-fluid w-100"
-              alt="image slot"
-              width="480"
-              height="1024"
-              src="@/assets/sea.jpg"
-            />
-          </template>
-        </b-carousel-slide>
-
-        <b-carousel-slide
-          caption="Hope you have a wonderful study experience 🤩!"
-        >
-          <template #img>
-            <img
-              class="d-block img-fluid w-100"
-              alt="image slot"
-              width="480"
-              height="1024"
-              src="@/assets/night.jpg"
-            />
-          </template>
-        </b-carousel-slide>
-      </b-carousel>
-    </div>
-    <div id="search-form">
-      <b-row class="justify-content-center">
-        <b-form v-if="show" @submit="onSubmit">
-          <b-form-group
-            id="input-group-1"
-            label-for="input-1"
-            description="Enter the course id or related terms you are searching for (e.g. ANTH 3)."
           >
-            <b-form-input
-              id="input-1"
-              v-model="form.course"
-              placeholder="Search Course"
-              required
-            />
-          </b-form-group>
-          <b-button id="submit" type="submit" variant="primary">
-            Submit
-          </b-button>
-        </b-form>
-        <ul id="chat-room-links">
-          <li v-for="link in links" :key="link._id">
-            <router-link :to="`/class/${link._id}`">
-              {{ link.courseID }}
-            </router-link>
-            <p>
-              {{ link.description }}
-            </p>
-          </li>
-        </ul>
-      </b-row>
-    </div>
+            <template #img>
+              <img
+                class="d-block img-fluid w-100"
+                alt="image slot"
+
+                src="@/assets/sea.jpg"
+              >
+            </template>
+          </b-carousel-slide>
+
+          <b-carousel-slide
+            caption="Hope you have a wonderful study experience 🤩!"
+          >
+            <template #img>
+              <img
+                class="d-block img-fluid w-100"
+                alt="image slot"
+
+                src="@/assets/night.jpg"
+              >
+            </template>
+          </b-carousel-slide>
+        </b-carousel>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="10">
+        <div id="search-form">
+          <b-form
+            v-if="show"
+            @submit="onSubmit"
+          >
+            <b-form-group
+              id="input-group-1"
+              label-for="input-1"
+              description="Enter the course id or related terms (e.g. ANTH 3)."
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.course"
+                placeholder="Search Course"
+                required
+              />
+            </b-form-group>
+            <b-button
+              id="submit"
+              type="submit"
+              variant="primary"
+            >
+              Submit
+            </b-button>
+          </b-form>
+          <ul id="chat-room-links">
+            <li
+              v-for="link in links"
+              :key="link._id"
+            >
+              <router-link :to="`/class/${link._id}`">
+                {{ link.courseID }}
+              </router-link>
+              <p>
+                {{ link.description }}
+              </p>
+            </li>
+          </ul>
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 // import axios from 'axios';
-import { axiosInstance } from "../utils/axiosInstance";
+import { axiosInstance } from '../utils/axiosInstance';
 // @ is an alias to /src
 export default {
-  name: "Home",
+  name: 'Home',
   components: {},
   data() {
     return {
       form: {
-        course: "",
+        course: '',
       },
-      titlemessage: "coursename: ",
+      titlemessage: 'coursename: ',
       links: [],
       show: true,
       slide: 0,
